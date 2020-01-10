2888fd812f92f842c3f5ca45491e37546d417854
tapizquent <Jose_Tapizquent@ultimatesoftware.com>
2020-01-10 09:40:52 -0500

    Modified MockPlayerService to return PLayer by specific id

--- src/app/services/mock-player.service.ts
+++ src/app/services/mock-player.service.ts
@@ -8,7 +8,15 @@ export class MockPlayerService {
 
   constructor() { }
 
+  private players: Player[] = [
+    new Player("907447", "Vanessa Yan"),
+    new Player("907429", "Asher Johnson Hogan"),
+    new Player("907443", "Reilly Markowitz"),
+    new Player("907444", "Ryan Scupp"),
+    new Player("907439", "Jose Tapizquent"),
+  ];
+
   getPlayerById(id: string): Player {
-    return new Player(id, "Jose T");
+    return this.players.filter(player => player.id == id)[0];
   }
 }
