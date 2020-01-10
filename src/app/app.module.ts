0fbe20cb30cd136fb3eb652bdd31daf2412b0eec
John Manos <John_Manos@ultimatesoftware.com>
2020-01-10 10:21:31 -0500

    Update matchservice and queuecomponent toward communicating with homecomponent and backend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QueueComponent } from './queue/queue.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { QueueComponent } from './queue/queue.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/queue/queue.component.ts
+++ src/app/queue/queue.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, ChangeDetectorRef } from '@angular/core';
 import { Player } from '../player';
 import { MatchService } from '../match.service';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
   styleUrls: ['./queue.component.css']
 })
-export class QueueComponent implements OnInit {
-  players: Player[] = Array();
-  
-  constructor(matchservice: MatchService) {
+export class QueueComponent implements OnInit, DoCheck {
+  players: Player[] = Array();  
+  subscription: Subscription;
+
+  constructor(private matchservice: MatchService, private ref: ChangeDetectorRef) {
+    this.matchservice = matchservice;
     var index = 0;
     for (let i in matchservice.match.teamOne) {
       this.players[index] = new Player(matchservice.match.teamOne[i].id, matchservice.match.teamOne[i].name);
@@ -20,9 +23,29 @@ export class QueueComponent implements OnInit {
       this.players[index] = new Player(matchservice.match.teamTwo[i].id, matchservice.match.teamTwo[i].name);
       index++;
     }
+    // This is one possible way I could periodically check the backend for queue changes
+    const checkInterval = interval(1000);
+    this.subscription = checkInterval.subscribe(res => console.log('hi'));
   }
 
   ngOnInit() {
   }
 
+  ngDoCheck() {
+    console.log('hi');
+    // This is another possible way I could periodically check the backend for queue changes
+    /*if (this.matchservice.getQueueStatus()) {
+      this.matchservice.getQueueStatus()
+        .subscribe(players => {this.players = players},
+          error => console.log('Failed to retrieve players from backend.' + error));
+    }*/
+    for (var p in this.players) {
+        this.players[p].name = Math.random() + '';
+    }
+    //this.ref.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
