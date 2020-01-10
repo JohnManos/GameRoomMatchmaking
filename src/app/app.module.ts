dfd4c81f51c7d7e9b2e19a5fdc455bbe6d2d39fa
tapizquent <Jose_Tapizquent@ultimatesoftware.com>
2020-01-09 19:14:31 -0500

    Created HomeComponent and added css layout. Added Material package to make it easier to display Modal Pop up for Game Choosing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { QueueComponent } from './queue/queue.component';
+import { HomeComponent } from './home/home.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeaderboardComponent,
-    QueueComponent
+    QueueComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
